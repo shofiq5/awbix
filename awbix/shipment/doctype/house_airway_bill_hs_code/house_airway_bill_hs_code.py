a4529172da7fa3c3f5f1267101bd64769fae580b
shofiq5 <shofiq5@gmail.com>
2026-06-19 19:41:54 +0300

    house_fix

--- awbix/shipment/doctype/house_airway_bill_hs_code/house_airway_bill_hs_code.py
+++ awbix/shipment/doctype/house_airway_bill_hs_code/house_airway_bill_hs_code.py
@@ -1,6 +1,9 @@
-import frappe
+# Copyright (c) 2026, Shofiq and contributors
+# For license information, please see license.txt
+
+# import frappe
 from frappe.model.document import Document
 
 
-class HouseAirwayBillHsCode(Document):
+class HouseAirwayBillHSCode(Document):
 	pass
